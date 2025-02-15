16cce268aca0c321009404914f3fff8891609c4b
Maddimanirakesh <rakesh25032002@gmail.com>
2025-02-15 16:57:13 +0530

    Adding Product entities

---
+++ src/main/java/com/example/git_demo/project/Product.java
@@ -0,0 +1,40 @@
+package com.example.git_demo.project;
+
+public class Product {
+    int id;
+    String productName;
+    String productColor;
+    String price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductColor() {
+        return productColor;
+    }
+
+    public void setProductColor(String productColor) {
+        this.productColor = productColor;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
